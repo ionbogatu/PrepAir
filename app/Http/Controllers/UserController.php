a81455c4c423b4189616cc52b8d61408d9a9056a
Ion Bogatu <ionbogatu@gmail.com>
2016-04-18 22:31:21 +0300

    database configuration

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: John Rich
+ * Date: 4/18/2016
+ * Time: 10:09 PM
+ */
+
+namespace App\Http\Controllers;
+
+class UserController extends Controller{
+
+}
