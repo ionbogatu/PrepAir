f4b774bb8ca4eea8ef5e5b059ac275aed2f68e05
Ion Bogatu <ionbogatu@gmail.com>
2016-06-12 16:00:16 +0300

    Added user's preferences + facebook login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,107 @@
 @extends('layouts.app')
 
 @section('content')
+<script>
+    function statusChangeCallback(response) {
+        //console.log('statusChangeCallback');
+        //console.log(response);
+        // The response object is returned with a status field that lets the
+        // app know the current login status of the person.
+        // Full docs on the response object can be found in the documentation
+        // for FB.getLoginStatus().
+        if (response.status === 'connected') {
+            // Logged into your app and Facebook.
+            // check if user has linked it's profile to our page
+            $.ajax({
+                'url': '/getUserByFBId',
+                'method': 'post',
+                'headers': {'X-CSRF-Token': $('meta[name=csrf-token]').attr('content')},
+                'data': {'response': response},
+                'dataType': 'json',
+                'success': function(data){
+                    if(data.response === 'connected'){
+                        // user is connected
+                        window.location.replace('/profile');
+                    }else if(data.response === 'not_linked'){
+                        // user is connected but his/her profile is not linked to our DB row
+                        $('.socnet-message').html('To unlock all functionalities of our platform, please <a href="/login">Login</a> or <a href="/register">Register</a>, after that link your profile to your facebook account').css({'display': 'block'});
+                    }else if(data.response === 'disconnected'){
+                        // user is not connected
+                    }
+                }
+            });
+            //testAPI();
+        } else if (response.status === 'not_authorized') {
+            // The person is logged into Facebook, but not your app.
+        } else {
+            // The person is not logged into Facebook, so we're not sure if
+            // they are logged into this app or not.
+        }
+    }
+
+    // This function is called when someone finishes with the Login
+    // Button.  See the onlogin handler attached to it in the sample
+    // code below.
+    function checkLoginState() {
+        FB.getLoginStatus(function(response) {
+            statusChangeCallback(response);
+        });
+    }
+
+    window.fbAsyncInit = function() {
+        FB.init({
+            appId      : '613779242110731',
+            cookie     : true,  // enable cookies to allow the server to access
+                                // the session
+            status     : true,  // get info about current user
+            xfbml      : true,  // parse social plugins on this page
+            version    : 'v2.5' // use graph api version 2.5
+        });
+
+        // Now that we've initialized the JavaScript SDK, we call
+        // FB.getLoginStatus().  This function gets the state of the
+        // person visiting this page and can return one of three states to
+        // the callback you provide.  They can be:
+        //
+        // 1. Logged into your app ('connected')
+        // 2. Logged into Facebook, but not your app ('not_authorized')
+        // 3. Not logged into Facebook and can't tell if they are logged into
+        //    your app or not.
+        //
+        // These three cases are handled in the callback function.
+
+        /*FB.getLoginStatus(function(response) {
+            statusChangeCallback(response);
+        });*/
+
+    };
+    // Load the SDK asynchronously
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk/debug.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+
+    // Here we run a very simple test of the Graph API after login is
+    // successful.  See statusChangeCallback() for when this call is made.
+    /*function testAPI() {
+        /*FB.api('/me', function(response) {
+         console.log('Successful login for: ' + response.name);
+         });*\/
+        FB.api('/me', function(response) {
+            console.log(JSON.stringify(response));
+            FB.getLoginStatus(function(response) {
+                if (response.status === 'connected') {
+                    var accessToken = response.authResponse.accessToken;
+                    console.log({'access_token': accessToken});
+                }
+            } );
+        });
+    }*/
+</script>
+
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
@@ -53,6 +154,28 @@
                                 <button type="submit" class="btn btn-primary">
                                     <i class="fa fa-btn fa-sign-in"></i>Login
                                 </button>
+                                <div class="social-networks-login-box">
+                                    <fb:login-button scope="public_profile,email" onlogin="checkLoginState();">
+                                    </fb:login-button>
+                                </div>
+                                <div class="g-signin2" data-onsuccess="onSignIn" data-theme="dark"></div>
+                                <script>
+                                    function onSignIn(googleUser) {
+                                        // Useful data for your client-side scripts:
+                                        var profile = googleUser.getBasicProfile();
+                                        console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+                                        console.log('Full Name: ' + profile.getName());
+                                        console.log('Given Name: ' + profile.getGivenName());
+                                        console.log('Family Name: ' + profile.getFamilyName());
+                                        console.log("Image URL: " + profile.getImageUrl());
+                                        console.log("Email: " + profile.getEmail());
+
+                                        // The ID token you need to pass to your backend:
+                                        var id_token = googleUser.getAuthResponse().id_token;
+                                        console.log("ID Token: " + id_token);
+                                    };
+                                </script>
+                                <div class="ui warning message socnet-message"></div>
                             </div>
                         </div>
                     </form>
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -9,170 +9,187 @@
 
 @extends ('layouts.layout')
 @section ('content')
+    <script>
+        function statusChangeCallback(response) {
+            //console.log('statusChangeCallback');
+            //console.log(response);
+            // The response object is returned with a status field that lets the
+            // app know the current login status of the person.
+            // Full docs on the response object can be found in the documentation
+            // for FB.getLoginStatus().
+            if (response.status === 'connected') {
+                // do nothing
+                // the facebook account is already linked to our app
+            } else if (response.status === 'not_authorized') {
+                $('.fb-link-item').css({'display': 'block'});
+            }
+        }
+
+        // This function is called when someone finishes with the Login
+        // Button.  See the onlogin handler attached to it in the sample
+        // code below.
+        function checkLoginState() {
+            FB.getLoginStatus(function(response) {
+                statusChangeCallback(response);
+            });
+        }
+
+        window.fbAsyncInit = function() {
+            FB.init({
+                appId      : '613779242110731',
+                cookie     : true,  // enable cookies to allow the server to access
+                                    // the session
+                status     : true,  // get info about current user
+                xfbml      : true,  // parse social plugins on this page
+                version    : 'v2.5' // use graph api version 2.5
+            });
+
+            // Now that we've initialized the JavaScript SDK, we call
+            // FB.getLoginStatus().  This function gets the state of the
+            // person visiting this page and can return one of three states to
+            // the callback you provide.  They can be:
+            //
+            // 1. Logged into your app ('connected')
+            // 2. Logged into Facebook, but not your app ('not_authorized')
+            // 3. Not logged into Facebook and can't tell if they are logged into
+            //    your app or not.
+            //
+            // These three cases are handled in the callback function.
+
+            FB.getLoginStatus(function(response) {
+                statusChangeCallback(response);
+            });
+
+        };
+        // Load the SDK asynchronously
+        (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = "//connect.facebook.net/en_US/sdk/debug.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+    </script>
     <br/>
     <br/>
-    <div class="ui container">
-        <div class="ui form">
+    <div class="ui container profile-manager">
+        <div class="ui form update-profile-data">
             <h4 class="ui dividing header">Personal information</h4>
             <div class="three fields">
                 <div class="field">
                     <label>Name</label>
                     <input name="name" placeholder="{{ $user->name }}" type="text">
                 </div>
-            </div>
-            <br/>
-            <div class="three fields">
-                <div class="field">
-                    <label>Password</label>
-                    <input name="password" value="password" type="password">
-                </div>
-                <div class="field">
-                    <label>Confirm password</label>
-                    <input name="confirm_password" placeholder="Confirm password" type="password">
+                <div class="field"></div>
+                <div class="field fb-link-item">
+                    <p>
+                        With your facebook account you can have access to all the functionalities that we offer.
+                        To link your facebook account with our application, please click the button below.
+                    </p>
                 </div>
             </div>
             <br/>
             <div class="three fields">
                 <div class="field">
-                    <input name="submit" class="ui teal button" value="Update profile" type="submit">
+                    <label>Change Password</label>
+                    <input name="password" placeholder="New Password" type="password">
+                </div>
+                <div class="field">
+                    <label>Confirm New Password</label>
+                    <input name="confirm_password" placeholder="Confirm New Password" type="password">
+                </div>
+                <div class="field fb-link-item">
+                    <label style="visibility: hidden;">Link with facebook</label>
+                    <button class="ui blue button linkWithFB">Link with facebook</button>
+                </div>
+            </div>
+            <br/>
+            <div class="three fields">
+                <div class="field">
+                    <input name="submit" class="ui teal button update-profile-btn" value="Update profile" type="button">
+                </div>
+            </div>
+            <div class="ui success message update-success">
+                <div class="header">
+                    Your profile has been updated successfully.
+                </div>
+            </div>
+            <div class="ui error message update-error">
+                <div class="header">
+                    Failed to update your profile.
                 </div>
             </div>
         </div>
-        <br/>
-        <br/>
         <h4 class="ui dividing header">Administrate preferences</h4>
-        <div class="preferences"></div>
+        <div class="preferences">
+            @if(isset($preferences))
+                <table class="ui selectable inverted table user-preferences">
+                    <thead>
+                    <tr>
+                        <th>Preference</th>
+                        <th>Value</th>
+                        <th class="right aligned">Actions</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($preferences as $preference)
+                        <tr id="{{ $preference->id }}">
+                            @if($preference->type_id == 1)
+                                <td>
+                                    Airport
+                                </td>
+                                <td>
+                                    {{ $preference->value1->country }}, {{ $preference->value1->name }}, {{ isset($preference->value1->iata_faa) ? $preference->value1->iata_faa : $preference->value1->icao }}
+                                </td>
+                                <td class="right aligned">
+                                    <!--i class="fa fa-pencil-square-o fa-lg update-preference" title="update"></i-->
+                                    <i class="fa fa-times fa-lg delete-preference" title="delete"></i>
+                                </td>
+                            @elseif($preference->type_id == 2)
+                                <td>
+                                    Airline
+                                </td>
+                                <td>
+                                    {{ $preference->value1->country }}, {{ $preference->value1->name }}, {{ isset($preference->value1->iata) ? $preference->value1->iata : $preference->value1->icao }}
+                                </td>
+                                <td class="right aligned">
+                                    <!--i class="fa fa-pencil-square-o fa-lg update-preference" title="update"></i-->
+                                    <i class="fa fa-times fa-lg delete-preference" title="delete"></i>
+                                </td>
+                            @elseif($preference->type_id == 3)
+                                <td>
+                                    Route
+                                </td>
+                                <td>
+                                    {{ $preference->value1->country }}, {{ $preference->value1->name }}, {{ isset($preference->value1->iata_faa) ? $preference->value1->iata_faa : $preference->value1->icao }}
+                                    <i class="dash fa fa-minus "></i>
+                                    {{ $preference->value2->country }}, {{ $preference->value2->name }}, {{ isset($preference->value2->iata_faa) ? $preference->value2->iata_faa : $preference->value2->icao }}
+                                </td>
+                                <td class="right aligned">
+                                    <!--i class="fa fa-pencil-square-o fa-lg update-preference" title="update"></i-->
+                                    <i class="fa fa-times fa-lg delete-preference" title="delete"></i>
+                                </td>
+                            @elseif($preference->type_id == 4)
+                                <td>
+                                    Stops
+                                </td>
+                                <td>
+                                    {{ $preference->value1 }}
+                                </td>
+                                <td class="right aligned">
+                                    <!--i class="fa fa-pencil-square-o fa-lg update-preference" title="update"></i-->
+                                    <i class="fa fa-times fa-lg delete-preference" title="delete"></i>
+                                </td>
+                            @endif
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            @endif
+        </div>
         <a href="#" class="add-new add-preference">+Add new</a>
         <br/>
-        <input name="submit" class="ui teal button" value="Save" type="submit">
+        <input name="submit" class="ui teal button submit-preferences" value="Save" type="submit">
         <div class="ui yellow message preference-message"></div>
-        <!--h4 class="ui dividing header">Added Routes</h4>
-        <a href="#" class="add-new add-route">+Add new</a>
-        <table class="ui selectable inverted table">
-            <thead>
-            <tr>
-                <th>N/o</th>
-                <th>Airline</th>
-                <th>Source</th>
-                <th>Destination</th>
-                <th>Departure time</th>
-                <th>Arrival time</th>
-                <th>Flight days</th>
-                <th>Stops</th>
-                <th>Note</th>
-                <th class="right aligned">Actions</th>
-            </tr>
-            </thead>
-            <tbody>
-            @if (empty($routes))
-                <tr>
-                    <td colspan="10" style="text-align: center;">You don't have any custom routes</td>
-                </tr>
-            @else
-                <?php $i = 1; ?>
-                @foreach($routes as $route)
-                    <?php
-                        $pretty_printed_days = '';
-                        $days = json_decode($route->days);
-                        foreach ($days as $day){
-                            switch($day){
-                                case 0: $pretty_printed_days .= 'Mon, ';
-                                    break;
-                                case 1: $pretty_printed_days .= 'Tue, ';
-                                    break;
-                                case 2: $pretty_printed_days .= 'Wed, ';
-                                    break;
-                                case 3: $pretty_printed_days .= 'Thu, ';
-                                    break;
-                                case 4: $pretty_printed_days .= 'Fri, ';
-                                    break;
-                                case 5: $pretty_printed_days .= 'Sat, ';
-                                    break;
-                                case 6: $pretty_printed_days .= 'Sun, ';
-                                    break;
-                            }
-                        }
-                        $pretty_printed_days = rtrim($pretty_printed_days, ', ');
-                    ?>
-                    <tr>
-                        <td>{{ $i++ }}</td>
-                        <td>{{ $route->airline }}</td>
-                        <td>{{ $route->source }}</td>
-                        <td>{{ $route->destination }}</td>
-                        <td>{{ $route->departure }}</td>
-                        <td>{{ $route->arrival }}</td>
-                        <td>{{ $pretty_printed_days }}</td>
-                        <td>{{ $route->stops }}</td>
-                        <td>{{ $route->relaxed_note }}</td>
-                        <td class="right aligned">
-                            <a href="#">Change</a>
-                            <span>|</span>
-                            <a href="#">Delete</a>
-                        </td>
-                    </tr>
-                @endforeach
-            @endif
-            </tbody>
-        </table>
-        <br/>
-        <br/>
-        <h4 class="ui dividing header">Added Airports</h4>
-        <p class="add-new add-airport">+Add new</p>
-        <table class="ui selectable inverted table">
-            <thead>
-            <tr>
-                <th>N/o</th>
-                <th>Status</th>
-                <th class="right aligned">Notes</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr>
-                <td>John</td>
-                <td>Approved</td>
-                <td class="right aligned">None</td>
-            </tr>
-            <tr>
-                <td>Jamie</td>
-                <td>Approved</td>
-                <td class="right aligned">Requires call</td>
-            </tr>
-            <tr>
-                <td>Jill</td>
-                <td>Denied</td>
-                <td class="right aligned">None</td>
-            </tr>
-            </tbody>
-        </table>
-        <br/>
-        <br/>
-        <h4 class="ui dividing header">Added Airlines</h4>
-        <p class="add-new add-airline">+Add new</p>
-        <table class="ui selectable inverted table">
-            <thead>
-            <tr>
-                <th>N/o</th>
-                <th>Status</th>
-                <th class="right aligned">Notes</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr>
-                <td>John</td>
-                <td>Approved</td>
-                <td class="right aligned">None</td>
-            </tr>
-            <tr>
-                <td>Jamie</td>
-                <td>Approved</td>
-                <td class="right aligned">Requires call</td>
-            </tr>
-            <tr>
-                <td>Jill</td>
-                <td>Denied</td>
-                <td class="right aligned">None</td>
-            </tr>
-            </tbody>
-        </table-->
     </div>
 @endsection
